6c82aa609cca453699ad2ce7dcc3210ba1af9895
gpminsight <90690274+gpminsight@users.noreply.github.com>
2023-07-27 16:37:07 +0100

    Script v2
    Script now adds all required fields to CSV, and is refactored for readability.

--- asana-export.py
+++ asana-export.py
@@ -1,8 +1,8 @@
 import asana
-import requests 
-import csv
+import requests
 import json
-import os
+import csv
+from datetime import datetime, timedelta
 
 with open('config.json', 'r') as f:
     config = json.load(f)
@@ -11,51 +11,61 @@ PERSONAL_ACCESS_TOKEN = config['api_key']
 WORKSPACE = config['workspace']
 
 client = asana.Client.access_token(PERSONAL_ACCESS_TOKEN)
+one_month_ago = (datetime.now() - timedelta(days=30)).isoformat()
 
-csv_filename = 'time_tracking_entries.csv'
-csv_headers = ['gid', 'resource_type', 'duration_minutes', 'created_by', 'entered_on']
+headers = {
+    "accept": "application/json",
+    "authorization": f"Bearer {PERSONAL_ACCESS_TOKEN}"
+}
 
-# Fetch all projects
-result = client.projects.get_projects({'workspace': WORKSPACE, 'archived': 'false'}, opt_pretty=True)
+projects = list(client.projects.get_projects({
+    'workspace': WORKSPACE,
+    'archived': 'false',
+    'opt_fields': 'name,gid,team.name,notes'
+}, opt_pretty=True))
 
-# Create a list of project gids
-project_gids = [project['gid'] for project in result]
+time_tracking_entries = []
 
-with open(csv_filename, 'w', newline='') as file:
-    writer = csv.DictWriter(file, fieldnames=csv_headers)
+for project in projects:
+    tasks = client.tasks.get_tasks({
+        'project': project['gid'],
+        'modified_since': one_month_ago,
+        'opt_fields': 'actual_time_minutes,notes,gid'
+    }, opt_pretty=True)
 
-    # Write the header
-    writer.writeheader()
+    for task in tasks:
+        actual_time = task.get('actual_time_minutes')
+        if actual_time is not None:
+            url = f"https://app.asana.com/api/1.0/tasks/{task['gid']}/time_tracking_entries?opt_fields=duration_minutes,entered_on,created_by,created_by.name"
+            entries = requests.get(url, headers=headers).json()['data']
 
-    # Loop through each project gid
-    for project_gid in project_gids:
-        # Fetch all tasks for the given project
-        tasks = client.tasks.get_tasks({'project': project_gid}, opt_pretty=True)
+            for entry in entries:
+                if entry['entered_on'] > one_month_ago:
+                    entry.update({
+                        'project_name': project['name'],
+                        'project_gid': project['gid'],
+                        'actual_time_minutes': actual_time,
+                        'team.name': project.get('team', {}).get('name', 'nil'),
+                        'project_notes': project.get('notes', '')
+                    })
+                    time_tracking_entries.append(entry)
 
-        # Extract task gids
-        task_gids = [task['gid'] for task in tasks]
+# Save to CSV
+with open('report.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['time_entry_id', 'employee_gid', 'employee_name', 'entered_on', 'project_name', 'project_gid', 'actual_time_minutes', 'company', 'NS Job ID'])
+    
+    for entry in time_tracking_entries:
+        writer.writerow([
+            entry['gid'],
+            entry['created_by']['gid'],
+            entry['created_by']['name'],
+            entry['entered_on'],
+            entry['project_name'],
+            entry['project_gid'],
+            entry['actual_time_minutes'],
+            entry['team.name'],
+            entry['project_notes']
+        ])
 
-        # For each task gid
-        for task_gid in task_gids:
-            url = "https://app.asana.com/api/1.0/tasks/" + task_gid + "/time_tracking_entries"
-
-            headers = {
-                "accept": "application/json",
-                "authorization": "Bearer " + PERSONAL_ACCESS_TOKEN
-            }
-
-            # Get the response
-            response = requests.get(url, headers=headers)
-
-            # Parse the response text into a dictionary
-            response_data = json.loads(response.text)
-
-            # Extract the actual data from the response (which is a list)
-            data_list = response_data.get('data', [])
-
-            # Write each dictionary in the list to the CSV file
-            for data in data_list:
-                # Extract the 'name' from the 'created_by' dictionary
-                created_by_name = data['created_by'].get('name')
-                data['created_by'] = created_by_name
-                writer.writerow(data)
+print("CSV report created successfully!")
